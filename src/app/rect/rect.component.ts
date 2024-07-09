de2af19e36a22560347a8e9f329c8df9592465e6
tuongtran1106 <tuongtran1106@gmail.com>
2024-07-09 16:57:36 +0700

    Using model function to setting up custom two-way-binding

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,12 +9,20 @@ import { Component, input, output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  size = input.required<{width: string; height: string}>();
-  sizeChange = output<{width: string; height: string}>();
+  // size = input.required<{width: string; height: string}>();
+  // sizeChange = output<{width: string; height: string}>();
+  size = model.required<{width: string; height: string}>();
+
   onReset() {
-    this.sizeChange.emit({
-      width: '200',
-      height: '100'
-    });
+    // this.sizeChange.emit({
+    //   width: '200',
+    //   height: '100'
+    // });
+    this.size.update(() => { // can also use set() method signal 
+      return {
+        height: '100',
+        width: '200'
+      }
+    })
   }
 }
